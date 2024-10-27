df941c5834be7758b37c8d84708fdd5bb290bf76
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-27 18:28:04 +0300

    removed httpresponse

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render
+import requests
+import datetime
 # Create your views here.
 def home(request):
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Kolkata'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=b452b74fd5752f39aad48dfce90e2b08'
+    PARAMS = {'units':'metric'}
     return render(request,'index.html') # Render the home.html template
